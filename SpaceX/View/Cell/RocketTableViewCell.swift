6729d4bc58a5582c88c883e511ecf92787ef6650
Habib Ghaffarzadeh <habibg@nuevo.com.tr>
2022-02-19 13:06:33 +0300

    Create RocketView and TableViewCell Design

---
+++ SpaceX/View/Cell/RocketTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RocketTableViewCell.swift
+//  SpaceX
+//
+//  Created by habib Ghaffarzadeh on 2/19/22.
+//
+
+import UIKit
+
+class RocketTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
